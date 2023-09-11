eb1bd4eb67cda1328b0421305da28e46dbb00a3c
L-Hyun <dkh1001@outlook.kr>
2023-09-11 23:58:35 +0900

    23.09.11 feat: 메인 페이지 작성 style: 폰트, global style 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Color Game",
@@ -16,11 +13,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko-kr">
-      <body className={inter.className}>
+      <body>
+        <header>The Color Game</header>
         <main>{children}</main>
         <footer>
-          이 페이지에는 (주)이키나게임즈가 제공한 'Ramche(램체)'가 적용되어
-          있습니다. <br />
+          <a href="https://m.blog.naver.com/campanula913/221366697603">
+            이 페이지에는 피나타가 제공한 PF스타더스트 가 적용되어 있습니다.
+          </a>
+          <br />
           Made by <a href="https://github.com/L-Hyun">L-Hyun</a>
         </footer>
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
 import React from "react";
+import styles from "./page.module.css";
+import Link from "next/link";
 
-type Props = {};
-
-const page = (props: Props) => {
-  return <div>Main</div>;
+const page = () => {
+  return (
+    <article className={styles.titleForm}>
+      <div>
+        <div className={styles.title}>The Color Game</div>
+        <div className={styles.subtitle}>틀린 색상 찾기</div>
+      </div>
+      <Link style={{ width: "100%" }} href={"/game"}>
+        <button className={`${styles.button} ${styles.startBtn}`}>
+          게임 시작
+        </button>
+      </Link>
+      <Link style={{ width: "100%" }} href={"/rank"}>
+        <button className={`${styles.button} ${styles.rankingBtn}`}>
+          랭킹 보기
+        </button>
+      </Link>
+    </article>
+  );
 };
 
 export default page;
